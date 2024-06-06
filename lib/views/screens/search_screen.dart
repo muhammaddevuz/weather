c0224c1d9ec6be5d2b32e89bc62251ed26e0cc52
Muhammad <149857879+muhammaddevuz@users.noreply.github.com>
2024-06-06 10:02:49 +0500

    final

--- lib/views/screens/search_screen.dart
+++ lib/views/screens/search_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather/controllers/weather_controller.dart';
 import 'package:weather/models/citys.dart';
+import 'package:weather/services/weather_http_service.dart';
 import 'package:weather/views/screens/home_screen.dart';
 
 // ignore: must_be_immutable
@@ -45,132 +46,117 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                    Color(0xFF1D2547),
-                    Color(0xFF1D2547),
-                    Color.fromARGB(255, 103, 63, 184),
-                    Color.fromARGB(255, 178, 123, 189),
-                  ])),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 50),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: TextField(
-                      onSubmitted: (value) async {
-                        enterButton();
-                      },
-                      controller: searchController,
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        label: const Text(
-                          "Shaxar nomini ingliz tilida kiriting",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                        suffixIcon: searchCheck
-                            ? IconButton(
-                                onPressed: enterButton,
-                                icon: const Icon(
-                                  Icons.search,
-                                  color: Colors.white,
-                                ))
-                            : SizedBox(
-                                width: 10,
-                                child: Image.asset("assets/load.gif"),
-                              ),
-                        errorText: searchError,
-                      ),
-                    ),
+      resizeToAvoidBottomInset: false,
+      body: Container(
+          padding: const EdgeInsets.all(20.0),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                Color(0xFF1D2547),
+                Color(0xFF1D2547),
+                Color.fromARGB(255, 103, 63, 184),
+                Color.fromARGB(255, 178, 123, 189),
+              ])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              TextField(
+                onSubmitted: (value) async {
+                  enterButton();
+                },
+                controller: searchController,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  label: const Text(
+                    "Shaxar nomini ingliz tilida kiriting",
+                    style: TextStyle(color: Colors.white),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                  suffixIcon: searchCheck
+                      ? IconButton(
+                          onPressed: enterButton,
+                          icon: const Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ))
+                      : SizedBox(
+                          width: 10,
+                          child: Image.asset("assets/load.gif"),
+                        ),
+                  errorText: searchError,
+                ),
+              ),
+              locationHistory.isNotEmpty
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        locationHistory.isNotEmpty
-                            ? Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    "Qidiruv tarixi",
-                                    style: TextStyle(
-                                        fontSize: 14, color: Colors.grey),
-                                  ),
-                                  TextButton(
-                                      onPressed: () {
-                                        locationHistory.clear();
-                                        saveLocationHistory(locationHistory);
-                                        setState(() {});
-                                      },
-                                      child: const Text(
-                                        "clear",
-                                        style: TextStyle(color: Colors.grey),
-                                      ))
-                                ],
-                              )
-                            : const SizedBox(),
-                        locationHistory.isNotEmpty
-                            ? Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  for (var i = 0;
-                                      i < locationHistory.length;
-                                      i++)
-                                    InkWell(
-                                      onTap: () {
-                                        Navigator.pushReplacement(
-                                            // ignore: use_build_context_synchronously
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) => HomeScreen(
-                                                  latLung: locationHistory[i]),
-                                            ));
-                                      },
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Text(
-                                            locationHistory[i],
-                                            style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.w600,
-                                                color: Colors.white),
-                                          ),
-                                          IconButton(
-                                              onPressed: () {
-                                                locationHistory.removeAt(i);
-                                                saveLocationHistory(
-                                                    locationHistory);
-                                                setState(() {});
-                                              },
-                                              icon: const Icon(
-                                                Icons.clear,
-                                                color: Colors.grey,
-                                              ))
-                                        ],
-                                      ),
-                                    )
-                                ],
-                              )
-                            : const SizedBox(),
+                        const Text(
+                          "Qidiruv tarixi",
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
+                        TextButton(
+                            onPressed: () {
+                              locationHistory.clear();
+                              saveLocationHistory(locationHistory);
+                              setState(() {});
+                            },
+                            child: const Text(
+                              "clear",
+                              style: TextStyle(color: Colors.grey),
+                            ))
                       ],
-                    ),
-                  )
-                ],
-              ))),
+                    )
+                  : const SizedBox(),
+              if (locationHistory.isNotEmpty)
+                Expanded(
+                  child: ListView.builder(
+                      padding: const EdgeInsets.symmetric(vertical: 0),
+                      itemCount: locationHistory.length,
+                      itemBuilder: (context, i) {
+                        return InkWell(
+                          onTap: () {
+                            locationHistory.insert(0, locationHistory[i]);
+                            locationHistory.removeAt(i + 1);
+                            Navigator.pushReplacement(
+                                // ignore: use_build_context_synchronously
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      HomeScreen(latLung: locationHistory[0]),
+                                ));
+                            saveLocationHistory(locationHistory);
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                locationHistory[i],
+                                style: const TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.white),
+                              ),
+                              IconButton(
+                                  onPressed: () {
+                                    locationHistory.removeAt(i);
+                                    saveLocationHistory(locationHistory);
+                                    setState(() {});
+                                  },
+                                  icon: const Icon(
+                                    Icons.clear,
+                                    color: Colors.grey,
+                                  ))
+                            ],
+                          ),
+                        );
+                      }),
+                )
+            ],
+          )),
       bottomNavigationBar: Container(
         color: const Color.fromARGB(255, 178, 123, 189),
         child: Row(
